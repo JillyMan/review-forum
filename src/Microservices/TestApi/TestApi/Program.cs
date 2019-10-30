74abfb0952895c076c121714c92ef0459529a2e8
Artsiom Dubinevich <Artsiom_Dubinevich@epam.com>
2019-10-30 15:33:27 +0300

    docker compouse plug to project

---
+++ src/Microservices/TestApi/TestApi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TestApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(args).Wait();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            using var client = new System.Net.Http.HttpClient();
+            var request = new System.Net.Http.HttpRequestMessage
+            {
+                RequestUri = new Uri("http://localhost:32769/WeatherForecast")
+            };
+            var response = await client.SendAsync(request);
+            var result = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(result);
+        }
+    }
+}
