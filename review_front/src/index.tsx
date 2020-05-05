5cda51f14c9ed4eadab1d25cbcc873503d37acce
SinkJ <boi54357@gmail.com>
2020-05-05 14:59:05 +0300

    yura
    init

--- review_front/src/index.tsx
+++ review_front/src/index.tsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import rootReducer from './store/reducers'
 import { AppContainer } from './components/container/app'
+import App from '../maket/app'
 
-const store = createStore(rootReducer, 
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__())
-    
-ReactDOM.render(<AppContainer store={store} />, document.getElementById('root'))
+// const store = createStore(rootReducer, 
+//     (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__())
+
+ReactDOM.render(<App></App>, document.getElementById('root'))
